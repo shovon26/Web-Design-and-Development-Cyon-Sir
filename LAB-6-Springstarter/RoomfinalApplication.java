2c442df75485c04ab8d15a95e19e58833b67cbf6
shovon26 <36441192+shovon26@users.noreply.github.com>
2019-04-15 23:41:40 +0600

    Create RoomfinalApplication.java

---
+++ LAB-6-Springstarter/RoomfinalApplication.java
@@ -0,0 +1,13 @@
+package edu.roomfinal.roomfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomfinalApplication.class, args);
+    }
+
+}
